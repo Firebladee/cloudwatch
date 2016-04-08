66d0f3dff9a814c6daa0f85aaad17d446d5a574a
Mark Greenhalgh <m.greenhalgh@elsevier.com>
2016-04-08 16:27:29 +0100

    Basic rspec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 describe 'cloudwatch' do
 
   context 'with defaults for all parameters' do
+    let(:params) {{
+      :metric_data  => :undef,
+      :metric_alarm => :undef,
+      :set_alarm    => :undef,
+      :metric_list  => :undef,
+    }}
+
     it { should contain_class('cloudwatch') }
+    it { is_expected.to compile }
+    it { is_expected.to have_resource_count(0) }
+    it { is_expected.to have_class_count(1) }
   end
 end
